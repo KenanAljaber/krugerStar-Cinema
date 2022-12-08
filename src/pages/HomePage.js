035fa9a27229ab4863e63505d304a38cc5a522b4
kenoisplaying <keno12333@hotmail.com>
2022-12-08 16:29:17 -0500

    agregando el banner del homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner";
+
+import "../styles/homePage.css"
+const HomePage = () => {
+    return ( 
+        <div className="homePageContainer">
+            <Banner title="Movies Mix" slogan="Your home of movies."/>
+            
+        </div>
+     );
+}
+ 
+export default HomePage;
