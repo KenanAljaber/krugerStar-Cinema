52add51821b083182d5f101ec95d76e49d02330e
kenoisplaying <keno12333@hotmail.com>
2022-12-08 18:24:32 -0500

    tratando de resolver el conflict

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import HomePage from "./pages/HomePage";
-import "./styles/index.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-       </Routes>
-       </Router>
+     
     </div>
   );
 }
