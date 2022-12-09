dd9d2c77650ceedb127de7b5a81af728ecdf735b
kenoisplaying <keno12333@hotmail.com>
2022-12-09 16:31:49 -0500

    agregando redux store y bottones de next y back al grid del homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import HomePage from "./pages/HomePage";
-import "./styles/index.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import store from "./store/MovieStore";
-import { Provider } from "react-redux";
 
 
 function App() {
-  
   return (
-    <Provider store={store}>
     <div className="App">
-      <Router>
-       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-       </Routes>
-       </Router>
+     
     </div>
-    </Provider>
   );
 }
 
