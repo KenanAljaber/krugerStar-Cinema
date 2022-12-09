b271122c79ae9c4581d16bbbaba19ef24e48ce65
Piguave <ariel_97_xd@hotmail.com>
2022-12-08 22:39:55 -0500

    Footer terminado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import HomePage from "./pages/HomePage";
-import "./styles/index.css"
-
-
+import "./styles/index.css";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+     <div className="App">
        <HomePage/>
     </div>
+    <Footer />
+    </>
+   
 
    
   );
