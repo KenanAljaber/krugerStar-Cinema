559baa400ade7a1057a0cb404e13e33320787e0b
Piguave <ariel_97_xd@hotmail.com>
2022-12-08 20:39:47 -0500

    Estructura del NavBar Creada y resolviendo conflictos en la rama

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import NavBar from "./components/NavBar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
 function App() {
   return (
-    <>
-    <NavBar />
+
      <div className="App">
         Cinema App
     </div>
-    </>
+
    
   );
 }
