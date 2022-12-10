9c4bd74e07cf653ed77c4e93d8d9ac05453bae68
Piguave <ariel_97_xd@hotmail.com>
2022-12-09 20:18:38 -0500

    Componente PageDetails terminado

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import "./styles/index.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import PageDetails from "./components/PageDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <NavBar />
-     <div className="App">
-       <HomePage/>
-    </div>
-    <Footer />
-    </>
-   
 
-   
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/Details" element={<HomePage />} />
+            <Route path="/" element={<PageDetails />} />
+          </Routes>
+          <Footer />
+        </Router>
+
+      </div>
+
+    </>
+
+
+
   );
 }
 
